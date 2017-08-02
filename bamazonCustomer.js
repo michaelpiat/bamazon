c5aab11625fed4d97abe6796d187b85ed9eefcd6
Michael Piatnichko <mpiatnichko@gmail.com>
2017-08-02 11:20:29 -0600

    display all avilable items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,4 +12,16 @@ connection.connect(function(err){
 	if (err) throw err; 
 
 	console.log("connected as id " + connection.threadId);
+	start();
 });
+
+function start() {
+	connection.query("SELECT * FROM products", function(err, res){
+		for (var i=0; i < res.length; i++) {
+			
+			console.log("Items avilable for sale: "+"\n"+"Item ID: "+res[i].item_id+"\n"+"Product: "+res[i].product_name
+				+"\n"+"Department: "+res[i].department_name+"\n"+"Price: $"+res[i].price+"\n"+"Quantity avilable: "+res[i].stock_quantity);
+			console.log("---------------");
+		}
+	})
+}
