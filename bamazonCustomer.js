947a646bb6a7d0ef73b37623c31b99eea3d28e2c
Michael Piatnichko <mpiatnichko@gmail.com>
2017-08-02 13:16:27 -0600

    most things work

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,5 +23,71 @@ function start() {
 				+"\n"+"Department: "+res[i].department_name+"\n"+"Price: $"+res[i].price+"\n"+"Quantity avilable: "+res[i].stock_quantity);
 			console.log("---------------");
 		}
-	})
+
+		inquirer.prompt([
+			{
+				name: "productId",
+				type: "input",
+				message: "Please enter the ID number of the product you wish to purchase: ",
+				validate: function(value) {
+          			if (isNaN(value) === false) {
+            			return true;
+          			}
+          				return false;
+        		}
+			},
+			{
+				name: "amount",
+				type: "input",
+				message: "Please enter the amount of units you wish to purchase: ",
+				validate: function(value) {
+          			if (isNaN(value) === false && value > 0) {
+            			return true;
+          			}
+          				return false;
+        		}
+			}
+		]).then(function(answer){
+
+			var userItem = res[answer.productId];			
+			
+			if(userItem.stock_quantity < answer.amount){
+				console.log("Insufficient quantity!");
+				start();
+			}
+			else {
+				
+				var updatedQty = parseInt(userItem.stock_quantity - answer.amount);
+				var purchaseTotal = parseFloat(userItem.price * answer.amount)
+				connection.query(
+					"UPDATE products SET ? WHERE ?",
+					[
+						{
+							stock_quantity: updatedQty
+						},
+						{
+							item_id: answer.productId
+						}
+					],
+					console.log("Your total is: $"+purchaseTotal+"\n"+"Thank you for shopping with us, please visit again soon!")
+					prompt,inquirer([
+						{
+							name:"buyMore",
+							type:"confirm",
+							message:"Would you like to purchase something else?"
+						}
+
+					]).then(function(answer){
+						if(answer === true){
+							start();
+						}
+						else {
+							process.exit();
+						}
+					});
+				);
+			}
+
+		});
+	});
 }
